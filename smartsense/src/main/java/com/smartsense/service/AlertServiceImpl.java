6423c742ef080443efb4e654d55c402940814530
anouar <anouar.ab95@gmail.com>
2024-12-18 14:47:28 +0100

    genner digramme de class

--- smartsense/src/main/java/com/smartsense/service/AlertServiceImpl.java
+++ smartsense/src/main/java/com/smartsense/service/AlertServiceImpl.java
@@ -11,10 +11,6 @@ import com.smartsense.service.interfaces.AlertService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Service implementation for Alert entity.
- * Defines methods for CRUD operations and additional business logic.
- */
 @Service
 @Slf4j
 @RequiredArgsConstructor
