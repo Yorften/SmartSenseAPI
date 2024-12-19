0b95a983c7d4680d75c0cdd759f4e8328f00b464
anouar <anouar.ab95@gmail.com>
2024-12-19 10:13:34 +0100

    SE-11 <fin gestion Lesures>

--- smartsense/src/main/java/com/smartsense/dto/alert/AlertDTO.java
+++ smartsense/src/main/java/com/smartsense/dto/alert/AlertDTO.java
@@ -1,14 +1,33 @@
 package com.smartsense.dto.alert;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.smartsense.dto.device.DeviceDTO;
+import com.smartsense.model.enums.Severity;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AlertDTO {
+    
+
+    @NotNull(message = "La sévérité ne peut pas être nulle")
+    private Severity severity;
+
+    @NotNull(message = "Le message ne peut pas être nul")
+    @Size(min = 5, max = 255, message = "Le message doit contenir entre 5 et 255 caractères")
+    private String message;
+
+    @NotNull(message = "Le timestamp ne peut pas être nul")
+    private LocalDateTime timestamp;
+  
+    @NotNull(message = "Le device ne peut pas être nul")
+
+    private DeviceDTO device;
 
 }
