8ef5ac535fab232a5c740f0333387b92b769f00f
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-12-19 16:25:19 +0100

    SE-15 security: added cron job to empty tokens in the blacklist that are safe to delete

--- smartsense/src/main/java/com/smartsense/repository/TokenBlacklistRepository.java
+++ smartsense/src/main/java/com/smartsense/repository/TokenBlacklistRepository.java
@@ -1,5 +1,7 @@
 package com.smartsense.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.smartsense.model.TokenBlacklist;
@@ -7,5 +9,7 @@ import com.smartsense.model.TokenBlacklist;
 public interface TokenBlacklistRepository extends MongoRepository<TokenBlacklist, String> {
 
     boolean existsByToken(String token);
+
+    long deleteByCreatedAtBefore(LocalDateTime date);
 
 }
